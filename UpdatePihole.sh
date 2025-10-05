8ad553632b59c772fcaacc152d498c421ceb0f02
gmr2048 <gmr2048@yahoo.com>
2025-10-05 09:52:53 -0400

    Update UpdatePihole.sh

--- UpdatePihole.sh
+++ UpdatePihole.sh
@@ -1,10 +1,10 @@
 pkill python3
-cd /home/gmr2048/Documents/PiHole/outfiles
+cd /tmp/PiHole/outfiles
 python3 -m http.server 8080 --bind 192.168.1.125 &
 #pihole -up
 sudo apt update
 sudo apt-get dist-upgrade -y
-/home/gmr2048/Documents/PiHole/PiholeAggAdlistUpdate.sh
+/tmp/PiHole/PiholeAggAdlistUpdate.sh
 sudo pihole -g
-rm /home/gmr2048/Documents/PiHole/outfiles/outfile
+rm /tmp/PiHole/outfiles/outfile
 pkill python3
